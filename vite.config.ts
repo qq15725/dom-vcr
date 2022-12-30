bbd4ed0ae478d099ad9b3a280ca9117fb983c155
wxm <157215725@qq.com>
2022-12-30 18:52:10 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve, basename } from 'path'
+import { defineConfig } from 'vite'
+import { name, browser, module, exports } from './package.json'
+
+const resolvePath = (str: string) => resolve(__dirname, str)
+
+export default defineConfig({
+  build: {
+    lib: {
+      formats: ['es', 'cjs', 'umd'],
+      fileName: format => {
+        if (format === 'es') return basename(module)
+        if (format === 'umd') return basename(browser)
+        if (format === 'cjs') return basename(exports['.'].require)
+        return `${ name }.${ format }`
+      },
+      entry: resolvePath('./src/index.ts'),
+      name,
+    },
+  },
+})
